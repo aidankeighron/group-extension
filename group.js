713de882aa4987ac3922d20ada586ff445867f3b
Aidan Keighron <78337317+SwervyK@users.noreply.github.com>
2023-07-16 02:31:44 -0400

    new ui

--- group.js
+++ group.js
@@ -1,17 +1,66 @@
-document.getElementById("save").addEventListener("click", saveGroup);
-document.getElementById("load").addEventListener("click", loadGroup);
+document.getElementById("clear").addEventListener("click", () => {
+  clearSave();
+});
+refreshUI();
 
-async function loadGroup() {
-  groupName = "Test";
+function addToTable(name, tableID) {
+  try {
+    var table = document.getElementById(tableID);
+    for (let i in table.rows) {
+      let row = table.rows[i];
+      for (let j in row.cells) {
+        let col = row.cells[j];
+        try {
+          if (col.innerHTML.includes(name)) {
+            return;
+          }
+        } catch (error) {}
+      }
+    }
+    var row = table.insertRow(-1);
+    // row.id = number+"queue";
+    row.insertCell(0).innerHTML =
+      '<td><button id="' + name + tableID + '">' + name + "</button></td>";
+    document.getElementById(name + tableID).addEventListener("click", () => {
+      if (tableID == "loadTable") {
+        loadGroup(name);
+      } else {
+        saveGroup(name);
+      }
+    });
+  } catch (error) {
+    // alert("Error");
+    console.log(error);
+  }
+}
+
+function refreshUI() {
+  console.log("Refreshing UI")
+  chrome.tabGroups.query({}, function (groups) {
+    groups.forEach(function (group) {
+      addToTable(group.title, "saveTable");
+    });
+  });
+
+  chrome.bookmarks.getTree(function (rootNode) {
+    rootNode[0].children[0].children.forEach(function (folder) {
+      if (folder.title == "GroupExtension") {
+        folder.children.forEach(function (group) {
+          addToTable(group.title, "loadTable");
+        });
+      }
+    });
+  });
+}
+
+async function loadGroup(groupName) {
+  console.log("Loading Group:" + groupName);
   chrome.tabs.create(
     { url: "https:\\asana.com", active: false },
     function (tab) {
-      chrome.tabs.group(
-        { tabIds: [tab.id] },
-        function (group) {
-          chrome.tabGroups.update(group, {title: groupName});
-        }
-      );
+      chrome.tabs.group({ tabIds: [tab.id] }, function (group) {
+        chrome.tabGroups.update(group, { title: groupName });
+      });
     }
   );
   chrome.bookmarks.getTree(function (rootNode) {
@@ -26,9 +75,10 @@ async function loadGroup() {
                   chrome.tabGroups.query(
                     { title: groupName, collapsed: false },
                     function (group) {
-                      chrome.tabs.group(
-                        { tabIds: [tab.id], groupId: group[0].id }
-                      );
+                      chrome.tabs.group({
+                        tabIds: [tab.id],
+                        groupId: group[0].id,
+                      });
                     }
                   );
                 }
@@ -41,61 +91,48 @@ async function loadGroup() {
   });
 }
 
-function saveGroup() {
-  const groupName = "Test";
-  const siteNames = [
-    "https:\\\\www.pcgamer.com",
-    "https:\\\\www.youtube.com",
-    "https:\\\\www.google.com",
-  ];
-  chrome.tabGroups.query(
-    { title: groupName },
-    function (group) {
-      chrome.tabs.query({groupId: group[0].id},
-        function (tabs) {
-          chrome.bookmarks.getTree(function (rootNode) {
-            rootNode[0].children[0].children.forEach(function (folder) {
-              if (folder.title == "GroupExtension") {
-                folder.children.forEach(function (group) {
-                  if (group.title == groupName) {
-                    chrome.bookmarks.removeTree(group.id);
-                  }
-                });
-                chrome.bookmarks.create(
-                  { parentId: folder.id, title: groupName },
-                  function (newFolder) {
-                    console.log(tabs);
-                    tabs.forEach(function (tab) {
-                      chrome.bookmarks.create({
-                        parentId: newFolder.id,
-                        title: tab.title,
-                        url: tab.url,
-                      });
-                    });
-                  }
-                );
+function clearSave() {
+  console.log("Clearing Save")
+  chrome.bookmarks.getTree(function (rootNode) {
+    rootNode[0].children[0].children.forEach(function (folder) {
+      if (folder.title == "GroupExtension") {
+        folder.children.forEach(function (group) {
+          chrome.bookmarks.removeTree(group.id);
+        });
+      }
+    });
+  });
+  refreshUI();
+}
+
+function saveGroup(groupName) {
+  console.log("Saving Group: " + groupName);
+  chrome.tabGroups.query({ title: groupName }, function (group) {
+    chrome.tabs.query({ groupId: group[0].id }, function (tabs) {
+      chrome.bookmarks.getTree(function (rootNode) {
+        rootNode[0].children[0].children.forEach(function (folder) {
+          if (folder.title == "GroupExtension") {
+            folder.children.forEach(function (group) {
+              if (group.title == groupName) {
+                chrome.bookmarks.removeTree(group.id);
               }
             });
-          });
-        })
-    }
-  );
-
-  // chrome.bookmarks.search("GroupExtension", function (matches) {
-  //     console.log(matches[0]);
-  //     chrome.bookmarks.create(
-  //         { parentId: matches[0].id, title: groupName },
-  //         function (newFolder) {
-  //             console.log("added folder: " + newFolder.title);
-  //             siteNames.forEach(function (site) {
-  //                 chrome.bookmarks.create({
-  //                     parentId: newFolder.id,
-  //                     title: site,
-  //                     url: site,
-  //                 });
-  //                 console.log("added site: " + site);
-  //             });
-  //         }
-  //     );
-  // });
+            chrome.bookmarks.create(
+              { parentId: folder.id, title: groupName },
+              function (newFolder) {
+                tabs.forEach(function (tab) {
+                  chrome.bookmarks.create({
+                    parentId: newFolder.id,
+                    title: tab.title,
+                    url: tab.url,
+                  });
+                });
+              }
+            );
+          }
+        });
+      });
+    });
+  });
+  refreshUI();
 }
